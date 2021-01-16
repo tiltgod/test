68cfdcc1988b0f136c0dde23a603f32e5297582a
tiltgod <59070084@it.kmitl.ac.th>
2021-01-16 21:08:47 +0700

    Re-write Asynchonous chat app and recap

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -1,45 +1,43 @@
 # chat/consumers.py
-# When Django accepts an HTTP request, 
-# it consults the root URLconf to lookup a view function, 
-# and then calls the view function to handle the request. 
-# Similarly, when Channels accepts a WebSocket connection, 
-# it consults the root routing configuration to lookup a consumer, 
-# and then calls various functions on the consumer to handle events from the connection.
+# The ChatConsumer that we have written is currently synchronous. 
+# Synchronous consumers are convenient because they can call regular synchronous I/O functions 
+# such as those that access Django models without writing special code. 
+# However asynchronous consumers can provide a higher level of performance since 
+# they don’t need to create additional threads when handling requests.
+
+# ChatConsumer only uses async-native libraries (Channels and the channel layer) 
+# and in particular it does not access synchronous Django models. 
+# Therefore it can be rewritten to be asynchronous without complications.
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-
-# This is a synchronous WebSocket consumer that accepts all connections, 
-# receives messages from its client, and echos those messages back to the same client. 
-# For now it does not broadcast messages to other clients in the same room.
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -48,10 +46,10 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
